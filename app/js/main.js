f67470cb873ceee7df64e1607ae84ebeaeabcdaa
10hokageee <nikita.serdyukov.20033002@gmail.com>
2023-08-05 22:50:31 +0300

    added desk version

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,11 @@
 
 $(function(){
   $('.reviews__list-items').slick({
+    prevArrow: '<button type="button" class="slick-next"><svg><use xlink:href="images/sprite.svg#arrow-left"></use></svg></button>',
+    nextArrow: '<button type="button" class="slick-prev"><svg><use xlink:href="images/sprite.svg#arrow-left"></use></svg></button>',
     slidesToShow: 3,
-    slidesToScroll: 3
+    slidesToScroll: 1,
+    infinite: true,
+    dots: true
   });
 });
